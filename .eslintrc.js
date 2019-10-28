809bbe676a65c5d8f9dd06b34e05f40f53d637b5
junqing258@gmail.com <junqing258@gmail.com>
2019-10-28 21:16:52 +0800

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  env: {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
   extends: [
       'plugin:@typescript-eslint/recommended',
       'react-app',
--- config/pnpTs.js
+++ config/pnpTs.js
@@ -1,35 +1,16 @@
-'use strict';
-
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { resolveModuleName } = require('ts-pnp');
 
-exports.resolveModuleName = (
-  typescript,
-  moduleName,
-  containingFile,
-  compilerOptions,
-  resolutionHost
-) => {
-  return resolveModuleName(
-    moduleName,
-    containingFile,
-    compilerOptions,
-    resolutionHost,
-    typescript.resolveModuleName
-  );
+exports.resolveModuleName = (typescript, moduleName, containingFile, compilerOptions, resolutionHost) => {
+  return resolveModuleName(moduleName, containingFile, compilerOptions, resolutionHost, typescript.resolveModuleName);
 };
 
-exports.resolveTypeReferenceDirective = (
-  typescript,
-  moduleName,
-  containingFile,
-  compilerOptions,
-  resolutionHost
-) => {
+exports.resolveTypeReferenceDirective = (typescript, moduleName, containingFile, compilerOptions, resolutionHost) => {
   return resolveModuleName(
     moduleName,
     containingFile,
     compilerOptions,
     resolutionHost,
-    typescript.resolveTypeReferenceDirective
+    typescript.resolveTypeReferenceDirective,
   );
 };
